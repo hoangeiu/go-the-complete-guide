aed04c79022eb3aee11c688dbd09514bdc9e290f
hoangeiu <leminhhoangk3@gmail.com>
2024-05-08 15:00:52 +0700

    implement multiple channels & goroutines

--- parallel/main.go
+++ parallel/main.go
@@ -5,18 +5,27 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	done := make(chan bool)
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
+	go slowGreet("How ... are ... you ...?", done)
+	go greet("I hope you're liking the course!", done)
+
+	for range done {
+		// fmt.Println(doneChange)
+	}
+
 }
