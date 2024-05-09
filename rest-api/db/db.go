0c95d998da9168787e2fd0c1d5bb1d58930818c8
hoangeiu <leminhhoangk3@gmail.com>
2024-05-09 23:02:09 +0700

    implement signup user

--- rest-api/db/db.go
+++ rest-api/db/db.go
@@ -27,6 +27,20 @@ func InitDB() {
 }
 
 func createTables() error {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		return err
+	}
+
 	createEventsTable := `
         CREATE TABLE IF NOT EXISTS events (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,10 +48,11 @@ func createTables() error {
             description TEXT NOT NULL,
             location TEXT NOT NULL,
             dateTime DATETIME NOT NULL,
-            user_id INTEGER
+            user_id INTEGER,
+			FOREIGN KEY(user_id) REFERENCES users(id)
         )
     `
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 	return err
 }
