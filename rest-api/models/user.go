731dc690588c1fe5db70206f7b0b0f7c3367d387
Lê Minh Hoàng <huongcaphemacchiato@gmail.com>
2024-06-04 18:35:48 +0700

    implement jwt method to generate jwt token

--- rest-api/models/user.go
+++ rest-api/models/user.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"errors"
+
 	"example.com/rest-api/db"
+	"example.com/rest-api/utils"
 )
 
 type User struct {
@@ -19,7 +22,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
@@ -29,4 +37,23 @@ func (u User) Save() error {
 
 	u.ID = userId
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT id, password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&u.ID, &retrievedPassword)
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
 }
--- rest-api/routes/routes.go
+++ rest-api/routes/routes.go
@@ -14,4 +14,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.DELETE("/events/:id", deleteEvent)
 
 	server.POST("/signup", signup)
+
+	server.POST("/login", login)
 }
